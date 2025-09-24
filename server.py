3dac5bfc82d1282027d5a09cb74cee03549c2181
Matheus-oliKelvin <matheuskelvin309@gmail.com>
2025-09-23 22:34:23 -0300

    alteracão 01.2

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import datetime
 
 app= Flask(__name__)
 #BANCO DE DADOS:
-senha='123'
+senha='123' #adm
 #origem, destino, id, motorista,quantAssentos,data, horarioSaida, horarioChegada, valor, assentos disponiveis
 viagens=[
           ['Sousa', 'Tenente Ananias','001','Luís', 40,  datetime.date(2025, 8, 28, ),datetime.time(5,45),datetime.time(6,30),345.99,
@@ -20,9 +20,10 @@ viagens=[
 #cpf[0]  assento_selecionado[1]
 reserva_assento=[]
 onibus=[['32332332', 40, 'Luís'], [ '2132323', 45,  'Rafael'] ] # [0] id, [1] assentos, [2] motorista
-passageiros=[]     #[0] nome  #[1] telefone   [2] cpf
+passageiros=[]     #[0] nome  #[1] cpf
 #login true ou false[0]  [1] cpf do passageiro
 login=[False,'null']
+
 
 
 @app.route('/')
@@ -91,9 +92,11 @@ def verificar_dados_passageiros():
 
 @app.route('/dados_viagens')
 def verificar_dados_viagens():
-    return render_template('/dados/dados_viagens.html', dados_viagens=viagens)
+    return render_template('/dados/dados_viagens.html', dados_viagens=viagens,reserva_dados=reserva_assento)
 
-
+@app.route('/pagar', methods=['post'])
+def fazer_pagamento():
+    return render_template('/homePage.html')
 @app.route('/buscar_rotas', methods=['post'])
 def buscar_rotas():
     global viagens
@@ -101,34 +104,58 @@ def buscar_rotas():
     destino=request.form.get('opcoes_viagem_destino')
     viagensmodif=[]
     verificador=False
+    contador=0
     for viagem in viagens:
         if origem== 'Sousa' and destino== 'Tenente Ananias':
             if viagem[0]== 'Sousa' and viagem[1]== 'Tenente Ananias':
                 viagensmodif.append(viagem)
                 verificador= True
+                contador=1
                 break
-            else:
+            elif contador == 0 :
                 verificador=False
         elif origem == 'Santa Cruz' and destino=='Vieropolis':
             if viagem[0]== 'Santa Cruz' and viagem[1]== 'Vieropolis':
                 viagensmodif.append(viagem)
                 verificador= True
-            else:
+                contador=1
+            elif  contador == 0 :
                 verificador=False
 
         else:
             verificador= False
+        contador=0
 
     if not verificador:
         return 'erro'
     else:
         return render_template('/viagens_filtradas.html',viagens=viagensmodif,reserva=reserva_assento)
 
-@app.route('/reservar_assento')
+@app.route('/reservar_assento', methods=['post'])
 def reservar_assento():
-    assentos_selecionados = request.form.getlist("assentos")
+    global reserva_assento
+    assentos_selecionados = request.form.getlist("assentos") #lista de assentos selecionados
+    id_viagem = request.form.get("id_viagem")
+    nomepassageiro= 'null'
+    preco_passagem=0
+    for passageiro in passageiros:
+        if passageiro[1] == login[1]:
+            nomepassageiro= passageiro[0]
+    if login[0]:
+        for viagem in viagens:
+            if viagem[2] == id_viagem:
+                preco_passagem=viagem[8]
+                for assento in assentos_selecionados:
+                        viagem[9].remove(assento)
+        if not reserva_assento:
+            reserva_assento=[[login[1],assentos_selecionados,id_viagem]]
+        else:
+            reserva_assento.append([login[1],assentos_selecionados,id_viagem])
+        return render_template('/pagamento_reserva.html',nome=nomepassageiro,assentos_select=len(assentos_selecionados), preco=preco_passagem)
 
 
+    else:
+        return render_template('/loginUser.html') #não ta logado
 
 
 if __name__  == '__main__':
--- static/viagens_abrir.js
+++ static/viagens_abrir.js
@@ -1,18 +1,7 @@
-function toggleCard() {
-      /* Declara a função chamada quando o cabeçalho é clicado.
-         Ela alterna o estado (fechado/aberto) do conteúdo. */
+function toggleCard(element) {
 
-      const conteudo = document.getElementById("viagem-conteudo");
-      /* Busca no DOM o elemento com id 'card-content' e guarda na constante 'content'. */
+      const conteudo = element.nextElementSibling;
 
       conteudo.classList.toggle("open");
-      /* Alterna a classe 'open' nesse elemento:
-         - Se a classe NÃO existir, adiciona (abrindo: aplica .card-content.open).
-         - Se a classe JÁ existir, remove (fechando: volta para .card-content).
-         A mudança da classe dispara a transição de 'max-height' definida no CSS.
-          content.classList.toggle("open") é como um interruptor de luz para a classe "open".
 
-        Se está apagada (classe ausente), ele acende (adiciona a classe).
-
-        Se está acesa (classe presente), ele apaga (remove a classe).*/
 }
